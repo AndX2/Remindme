09e5767bc5244fbc8ce93d0fa590da460c9153a6
And2X <Andrew.android.yandex@gmail.com>
2016-02-01 12:19:52 +0300

    Create ActivityMain and layout for it

---
+++ app/src/main/java/ru/yandex/android/andrew/remindme/ActivityMain.java
@@ -0,0 +1,17 @@
+package ru.yandex.android.andrew.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Andrew on 01.02.2016.
+ */
+public class ActivityMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
